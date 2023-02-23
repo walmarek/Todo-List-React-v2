d73d451a4cbb564992a7af8f76a2902f75bce5d7
Marek Wal <walmarek1987@gmail.com>
2023-02-23 21:48:49 +0100

    styledNavLinks added

---
+++ src/styled.js
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+const activeClassName = "active"
+
+export const StyledNavLink = styled(NavLink).attrs(() => ({
+    activeClassName,
+}))`
+&.${activeClassName} {
+    color: red;
+}`
