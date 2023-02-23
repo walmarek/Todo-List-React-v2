5909c4d7a49c5fc9d3be83b9938084e3ee435159
Marek Wal <walmarek1987@gmail.com>
2023-02-23 22:02:04 +0100

    react-router HashRouter added to App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,39 @@
-import Tasks from "./features/tasks/Tasks"
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import AuthorPage from "./features/author/AuthorPage";
+import TaskPage from "./features/tasks/TaskPage";
+import TasksPage from "./features/tasks/TasksPage/index..js";
+import { StyledNavLink } from "./styled";
 
-export const App = () => <Tasks />;
+
+export const App = () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <StyledNavLink to="/tasks">
+            To-do list
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/author">
+            About author
+          </StyledNavLink>
+        </li>
+      </ul>
+      <Switch>
+      <Route path="/tasks/:id">
+          <TaskPage />
+        </Route>
+        <Route path="/tasks">
+          <TasksPage />
+        </Route>
+        <Route path="/author">
+          <AuthorPage />
+        </Route>
+        <Route path="/">
+          <Redirect to="tasks" />
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+);
