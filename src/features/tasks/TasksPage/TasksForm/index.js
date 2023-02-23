a30ffdaee868871604c49ec09dfa2f02acbcb928
Marek Wal <walmarek1987@gmail.com>
2023-02-23 21:57:16 +0100

    source changed

--- src/features/tasks/TasksPage/TasksForm/index.js
+++ src/features/tasks/TasksPage/TasksForm/index.js
@@ -2,7 +2,7 @@ import { nanoid } from "@reduxjs/toolkit";
 import { useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { Form, Input, Button } from "./styled";
-import { addTask } from "../tasksSlice";
+import { addTask } from "../../tasksSlice";
 
 const TasksForm = () => {
   const [newTaskContent, setNewTaskContent] = useState("");
