f9e4a1655893a3aaff2e43f96d84286a6a69b5f9
Marek Wal <walmarek1987@gmail.com>
2023-02-27 20:46:03 +0100

    taskPage styled component

---
+++ src/features/tasks/TaskPage/TextArea/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Field = styled.div`
+margin: 20px;
+margin-top: 10px;
+`
+export const Section = styled.span`
+display: flex;
+justify-content: end;
+margin-right: 14px;
+`
