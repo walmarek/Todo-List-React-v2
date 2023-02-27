0b85d8b85ce38b17721e926c52831ca38557a06b
Marek Wal <walmarek1987@gmail.com>
2023-02-27 21:01:52 +0100

    search filtering tasks

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,32 +1,48 @@
-import { useHistory, useLocation } from "react-router-dom";
 import Input from "../../Input";
+import {
+  useQueryParameter,
+  useReplaceQueryParameter,
+} from "../../queryParameters";
 import searchQueryParamName from "../searchQueryParamName";
 import { Wrapper } from "./styled";
 
 // eslint-disable-next-line
 export default () => {
-  const location = useLocation();
-  const history = useHistory();
-  const query = new URLSearchParams(location.search).get(searchQueryParamName);
+  // using hooks ...queryParameters
+  const query = useQueryParameter(searchQueryParamName);
+  const replaceQueryParameter = useReplaceQueryParameter();
 
   const onInputChange = ({ target }) => {
-    const searchParams = new URLSearchParams(location.search);
-    if (target.value.trim() === "") {
-      searchParams.delete(searchQueryParamName);
-    } else {
-      searchParams.set(searchQueryParamName, target.value);
-    }
-
-    history.push(`${location.pathname}?${searchParams.toString()}`);
+    replaceQueryParameter({
+      key: searchQueryParamName,
+      value: target.value.trim() !== "" ? target.value : undefined,
+    });
   };
+
+  // code without hooks
+
+  // const location = useLocation();
+  // const history = useHistory();
+  // const query = new URLSearchParams(location.search).get(searchQueryParamName);
+
+  // const onInputChange = ({ target }) => {
+  //   const searchParams = new URLSearchParams(location.search);
+  //   if (target.value.trim() === "") {
+  //     searchParams.delete(searchQueryParamName);
+  //   } else {
+  //     searchParams.set(searchQueryParamName, target.value);
+  //   }
+
+  //   history.push(`${location.pathname}?${searchParams.toString()}`);
+  // };
 
   return (
     <Wrapper>
-        <Input
-      placeholder="Filter tasks"
-      value={query || ""}
-      onChange={onInputChange}
-    />
+      <Input
+        placeholder="Filter tasks"
+        value={query || ""}
+        onChange={onInputChange}
+      />
     </Wrapper>
   );
 };
