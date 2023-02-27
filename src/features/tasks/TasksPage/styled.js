290714965746e539b1c98da2671e85eee71e0595
Marek Wal <walmarek1987@gmail.com>
2023-02-27 20:47:13 +0100

    taskPage styled-componenents added

---
+++ src/features/tasks/TasksPage/styled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const ListButtons = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const Button = styled.button`
+  margin: 4px;
+  padding: 4px;
+  border: none;
+  cursor: pointer;
+  color: teal;
+  transition: 0.5s;
+  background: transparent;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    margin: 5px;
+    background: transparent;
+  }
+
+  &:hover {
+    color: hsl(180, 100%, 35%);
+  }
+
+  &:active {
+    color: hsl(180, 100%, 45%);
+  }
+
+  &:disabled {
+    color: #bbb;
+    cursor: default;
+  }
+`;
