bf334d1637d5a8848ad09dabad0d4936c5bd9660
Marek Wal <walmarek1987@gmail.com>
2023-02-27 20:40:18 +0100

    hashRouter & navBar created

--- src/App.js
+++ src/App.js
@@ -2,38 +2,37 @@ import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import AuthorPage from "./features/author/AuthorPage";
 import TaskPage from "./features/tasks/TaskPage";
 import TasksPage from "./features/tasks/TasksPage/index..js";
-import { StyledNavLink } from "./styled";
-
+import { Img, NavBar, NavItem, NavList, StyledNavLink } from "./styled";
+import logo from "./logo.ico";
 
 export const App = () => (
   <HashRouter>
-    <nav>
-      <ul>
-        <li>
-          <StyledNavLink to="/tasks">
-            To-do list
-          </StyledNavLink>
-        </li>
-        <li>
-          <StyledNavLink to="/author">
-            About author
-          </StyledNavLink>
-        </li>
-      </ul>
-      <Switch>
+    <NavBar>
+      <StyledNavLink to="/tasks">
+        <Img src={logo} alt="logo" />
+      </StyledNavLink>
+      <NavList>
+        <NavItem>
+          <StyledNavLink to="/tasks">To-do list</StyledNavLink>
+        </NavItem>
+        <NavItem>
+          <StyledNavLink to="/author">About author</StyledNavLink>
+        </NavItem>
+      </NavList>
+    </NavBar>
+    <Switch>
       <Route path="/tasks/:id">
-          <TaskPage />
-        </Route>
-        <Route path="/tasks">
-          <TasksPage />
-        </Route>
-        <Route path="/author">
-          <AuthorPage />
-        </Route>
-        <Route path="/">
-          <Redirect to="tasks" />
-        </Route>
-      </Switch>
-    </nav>
+        <TaskPage />
+      </Route>
+      <Route path="/tasks">
+        <TasksPage />
+      </Route>
+      <Route path="/author">
+        <AuthorPage />
+      </Route>
+      <Route path="/">
+        <Redirect to="tasks" />
+      </Route>
+    </Switch>
   </HashRouter>
 );
